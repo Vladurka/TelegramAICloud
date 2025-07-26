aa7240eea93bfa32f671dd4e6ff247f6c9842c7c
Vladurka <0986535843v@gmail.com>
2025-07-26 16:04:31 +0200

    dada

--- javascript/src/errors/RabbitMQNotConnectedError.js
+++
@@ -1,7 +0,0 @@
-export const RabbitMQNotConnectedError = class extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "RabbitMQNotConnectedError";
-    Error.captureStackTrace(this, RabbitMQNotConnectedError);
-  }
-};
