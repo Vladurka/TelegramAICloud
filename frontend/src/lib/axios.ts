bf02efef3b4ca4b64fefa4b48bc36c2099a5fcf9
Vladurka <0986535843v@gmail.com>
2025-08-13 16:23:26 +0200

    nginx update

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL:
+    import.meta.env.VITE_ENV === "production"
+      ? import.meta.env.VITE_PROD_SERVER_URL
+      : import.meta.env.VITE_DEV_SERVER_URL,
 });
